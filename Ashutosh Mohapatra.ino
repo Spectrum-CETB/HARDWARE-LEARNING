9ddcfaa74717f0d52c257f971ad6e66a789621c1
bpn123 <63866874+bpn123@users.noreply.github.com>
2020-11-01 20:41:27 +0530

    Update Ashutosh Mohapatra.ino

--- Ashutosh Mohapatra.ino
+++ Ashutosh Mohapatra.ino
@@ -1,1 +1,15 @@
-
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Temperature in C");
+}
+void loop() {
+  float t;
+  float input = analogRead(A5);
+  input=input/1024;
+  input=input*5;
+  t=(input-0.4971)*100;
+  lcd.setCursor(0,1);
+  lcd.print(t);
+}
