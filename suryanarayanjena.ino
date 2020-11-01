800dc3c6408ff7945c01680eda14a5a20d4035cc
Surya Narayan Jena <30215820+atulsnjena@users.noreply.github.com>
2020-11-01 11:23:18 +0530

    Add Task 1 solutions
    Solution added.

--- suryanarayanjena.ino
+++ suryanarayanjena.ino
@@ -1,1 +1,15 @@
-
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Temperature in C");
+}
+void loop() {
+  float temp;
+  float read = analogRead(A0);
+  read=read/1024;
+  read=read*5;
+  temp=(read-0.4971)*100;
+  lcd.setCursor(0,1);
+  lcd.print(temp);
+}
